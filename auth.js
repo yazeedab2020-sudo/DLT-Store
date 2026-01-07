7b9ecaad5a8f205e0f6b5d3315f1e2542f8ae267
DLT Store <yazeed.ab2020@gmail.com>
2026-01-08 01:43:25 +0300

    Add files via upload

--- auth.js
+++ auth.js
@@ -58,7 +58,11 @@ function renderAccountArea() {
         }
     } else {
         container.innerHTML = `
-            <a href="login.html" class="bg-neonGreen/10 text-neonGreen border border-neonGreen/30 px-3 py-1 rounded-lg text-xs md:text-sm hover:bg-neonGreen/20 transition">تسجيل الدخول</a>
+            <a href="login.html" class="hover:text-neonBlue transition transform hover:scale-110" title="تسجيل الدخول">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                </svg>
+            </a>
         `;
     }
 }
